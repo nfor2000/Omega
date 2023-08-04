caff8c681f705273fcedfbde7e458f08d39ad6fe
nfor2000 <nforpresly2@gmail.com>
2023-08-04 05:40:50 +0100

    update

--- js/index.js
+++ js/index.js
@@ -6,13 +6,18 @@ const CloseMenu = document.querySelector(".close-menu");
 
 const navLink = document.querySelectorAll(".nav_link");
 
+var slideIndex = 0;
 
 
 menuBar.addEventListener("click", function () {
      navMenu.classList.add("active");
+     document.querySelector(".slideshow_container").style.zIndex = "-2"
 })
 CloseMenu.addEventListener("click", function () {
      navMenu.classList.remove("active");
+     setTimeout(function(){
+          document.querySelector(".slideshow_container").style.zIndex = "0"
+     },300)
 })
 
 navLink.forEach(link => {
@@ -20,4 +25,22 @@ navLink.forEach(link => {
           navMenu.classList.remove("active");
      })
 })
+
+slideShow()
+function slideShow(){
+     const slides = document.getElementsByClassName("slide_show");
+
+     for(var i = 0; i < slides.length; i ++)
+     {
+          slides[i].style.display = "none";
+     }
+
+     slideIndex ++;
+     if(slideIndex > slides.length){
+          slideIndex = 1;
+     }
+     slides[slideIndex-1].style.display = "flex";
+
+     setTimeout(slideShow,3000);
+}
 
